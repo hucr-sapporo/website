f0fd1312121682c080ae4b2d51dea97a1b17e679
Kogia-sima <orcinus4627@gmail.com>
2019-05-16 03:55:23 +0900

    external link icon

--- src/App.vue
+++ src/App.vue
@@ -12,9 +12,18 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+a {
+  color: green;
+
+  &.external:after {
+    font-family: "fontello";
+    content: '\f08e';
+    margin-left: 5px;
+  }
+}
+
 #app {
   font-family:'Lato', 'Noto Sans JP', '游ゴシック Medium', '游ゴシック体', 'Yu Gothic Medium', YuGothic, 'ヒラギノ角ゴ ProN', 'Hiragino Kaku Gothic ProN', 'メイリオ', Meiryo, 'ＭＳ Ｐゴシック', 'MS PGothic', sans-serif;
-  text-align: center;
 }
 </style>

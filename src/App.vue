dde693648b0a1d2868e7fe2d11d19b7fd0bd9b30
Kogia-sima <orcinus4627@gmail.com>
2019-05-16 10:10:36 +0900

    Page transition animation

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
   <main id="app">
-    <router-view/>
+    <top-bar ref="topBar"></top-bar>
+    <transition name="page" mode="out-in">
+      <router-view/>
+    </transition>
   </main>
 </template>
 
 <script>
 import 'normalize.css';
+import TopBar from './components/TopBar.vue';
 
 export default {
   name: 'App', // for devtool
+  components: {
+    TopBar
+  },
+  mounted() {
+    console.log(this);
+  }
 }
 </script>
 
@@ -25,5 +35,12 @@ a {
 
 #app {
   font-family:'Lato', 'Noto Sans JP', '游ゴシック Medium', '游ゴシック体', 'Yu Gothic Medium', YuGothic, 'ヒラギノ角ゴ ProN', 'Hiragino Kaku Gothic ProN', 'メイリオ', Meiryo, 'ＭＳ Ｐゴシック', 'MS PGothic', sans-serif;
+}
+
+.page-enter-active, .page-leave-active {
+  transition: opacity 0.5s;
+}
+.page-enter, .page-leave-to {
+  opacity: 0;
 }
 </style>
